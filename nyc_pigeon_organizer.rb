0ff4d883d7e636576e0dd8c13e7a191e6c295ded
Alice Wang <aywang6@gmail.com>
2020-04-02 15:48:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,14 @@
 def nyc_pigeon_organizer(data)
   
+  index = 0 
+  while index < data.length 
   
+    inner_index = 0 
+    while inner_index < data[index].length
+  
+      element = 0 
+  while element < data[index][inner_index].length 
+  
+  pigeon_name = [data]
   
 end
