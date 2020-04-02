a589a245d168f5c56b0784fcafb68f8a5314e3a5
Alice Wang <aywang6@gmail.com>
2020-04-02 16:10:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,11 @@ def nyc_pigeon_organizer(data)
   
   data_by_pigeon = {}
   
-  data.keys.each do |key| 
-    puts data[key].keys
+  data.keys.each do |property| 
+    data[property].keys.each do |value|
+      data[property][value]
+    end
   end
-    
  
   
 end
